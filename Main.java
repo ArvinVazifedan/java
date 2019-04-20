7546ea5adb1efc4ac906d44b45774bf1c096652a
arvin <rwinvaz@gmail.com>
2019-04-20 14:01:24 +0430

    add first class to rep

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+class Main{
+   public static void main(String[] args){
+       String[] names = {"Anouy", "Soiuli", "touka"};
+       for(String n : names){
+           System.out.print(n);
+       }
+   }
+}
