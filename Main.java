7c7a9e37c90a800f3801e40d7377da3d83b02528
arvin <rwinvaz@gmail.com>
2019-04-20 14:13:07 +0430

    add int array for to class

--- Main.java
+++ Main.java
@@ -2,8 +2,12 @@
 class Main{
    public static void main(String[] args){
        String[] names = {"Anouy", "Soiuli", "touka"};
+       int[] nums = {1, 2, 3, 4};
        for(String n : names){
            System.out.print(n);
+       }
+       for(int nu : nums){
+           System.out.print(nu);
        }
    }
 }
